609a5b40feed3df1c0943adfaed2d4ec91422883
Bojan Bakaric <bakaricbojan@gmail.com>
2023-03-27 15:19:08 +0000

    First test

---
+++ scripts/calc.js
@@ -0,0 +1,16 @@
+function addition() {
+    return 42;
+}
+
+module.exports = addition;
+
+
+
+// num1 = 20
+// num2 = 22
+
+// function calculator(num1, num2) {
+//     result = num1 + num2
+//     console.log(result)
+//     return result
+// }
